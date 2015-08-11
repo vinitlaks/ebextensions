66834120fd99d78a242b49696050659e5cb57f04
Vineeth <lvineeth@amazon.com>
2015-08-11 15:39:30 -0700

    Testing travis - Using actual s3 credentials

--- script.py
+++ script.py
@@ -1,6 +1,22 @@
 import os
+import sys
+import zipfile
+import botocore.session
 
-print os.environ['access_key_id']
-print os.environ['secret_access_key']
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+zipf = zipfile.ZipFile('testfolder.zip', 'w')
+zipdir('./testfolder', zipf)
+zipf.close()
 
 
+file_path = './testfolder.zip'
+fp = open(file_path, 'rb')
+session = botocore.session.get_session()
+session.set_credentials(os.environ['access_key_id'],os.environ['secret_access_key'])
+client = session.create_client('s3', region_name='us-west-2')
+client.put_object(Bucket=os.environ['bucket_name'],Body=fp,Key='testfolder.zip')
