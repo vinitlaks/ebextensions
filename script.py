ebfc4f62988b1ccd2786d0f16fe850b071f27af1
Vineeth <lvineeth@amazon.com>
2015-08-12 09:34:39 -0700

    Actual ebextensions folder

--- script.py
+++ script.py
@@ -9,14 +9,14 @@ def zipdir(path, ziph):
         for file in files:
             ziph.write(os.path.join(root, file))
 
-zipf = zipfile.ZipFile('testfolder.zip', 'w')
-zipdir('./testfolder', zipf)
+zipf = zipfile.ZipFile('ebextensions.zip', 'w')
+zipdir('./ebextensions', zipf)
 zipf.close()
 
 
-file_path = './testfolder.zip'
+file_path = './ebextensions.zip'
 fp = open(file_path, 'rb')
 session = botocore.session.get_session()
 session.set_credentials(os.environ['access_key_id'],os.environ['secret_access_key'])
 client = session.create_client('s3', region_name='us-west-2')
-client.put_object(Bucket=os.environ['bucket_name'],Body=fp,Key='testfolder.zip')
+client.put_object(Bucket=os.environ['bucket_name'],Body=fp,Key='ebextensions.zip')
